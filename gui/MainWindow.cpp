0694ec4a86674a9dc696166a9338ff30ad717dc5
Petr Mrázek <peterix@gmail.com>
2015-02-05 22:07:47 +0100

    SCRATCH remove legacy instances

--- gui/MainWindow.cpp
+++ gui/MainWindow.cpp
@@ -76,7 +76,6 @@
 
 #include "logic/InstanceList.h"
 #include "logic/minecraft/MinecraftVersionList.h"
-#include "logic/minecraft/LwjglVersionList.h"
 #include "logic/icons/IconList.h"
 #include "logic/java/JavaVersionList.h"
 
@@ -308,10 +307,6 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 		{
 			m_versionLoadTask = MMC->minecraftlist()->getLoadTask();
 			startTask(m_versionLoadTask);
-		}
-		if (!MMC->lwjgllist()->isLoaded())
-		{
-			MMC->lwjgllist()->loadList();
 		}
 
 		m_newsChecker->reloadNews();
